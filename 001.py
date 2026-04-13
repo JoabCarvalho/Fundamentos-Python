6ab177cfc53ca9e426f6a63af11116026051a823
JoabCarvalho <carvalho.morais.sales@gmail.com>
2026-04-13 11:44:43 -0300

    Envio Arquivo 001.py

---
+++ 001.py
@@ -0,0 +1,8 @@
+# Programa que lê duas notas e calcula a média delas
+
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print(f"A média da soma das notas {nota1} + {nota2} é: {media}!")
